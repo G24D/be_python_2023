025ba74cb4b1af6eeae1b3b4543f4bb4737c2050
G24D <ssktt.aye@gmail.com>
2024-02-21 18:02:00 +0800

    k

---
+++ Django_Project/postagram-frontend-vite/src/main.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './routes/root.jsx'
+import Register from './routes/register.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
